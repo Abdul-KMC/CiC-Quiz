34d413df53cb9ea0bba5ab85d2a03ba810c45ad6
Abdul-KMC <mfnadwi0522@gmail.com>
2024-03-26 05:41:53 +0530

    feat: environmental setup for front-end testing

---
+++ client/src/tests/quiz.test.js
@@ -0,0 +1,4 @@
+ import "@testing-library/jest-dom";
+ import { fireEvent, render, screen } from "@testing-library/react";
+ import userEvent from "@testing-library/user-event";
+ import Convertor from "../components/Convertor";
