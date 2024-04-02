dd24021f11ceecdc27a35d5bf27395eeafc5cd5f
Abdul-KMC <mfnadwi0522@gmail.com>
2024-04-02 06:32:23 +0530

    feat: added testing table option

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const mongoString = process.env.DATABASE_URL;
+const mongoString = process.env.NODE_ENV === "test" ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL
 const routes = require('./routers/routes');
 const questionRoutes = require('./routers/questionRoutes');
 const userRoute = require('./routers/userRoute');
